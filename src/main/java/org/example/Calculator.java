6f20c49b51a37828cc7cfb8fdcad417a2413c155
amita patel <amita70007@gmail.com>
2021-05-16 01:29:15 +0100

    empty test case

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,5 +4,6 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("this is calculator class");
+        System.out.println("new added statement");
     }
 }
